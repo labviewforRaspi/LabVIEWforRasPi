2b30540f4a15efd8670634a0fa826e236155d1c4
dkinion <darin@alum.mit.edu>
2016-05-22 15:10:38 -0700

    fixed smbus install

--- install/install.sh
+++ install/install.sh
@@ -10,7 +10,7 @@ pip3 install autobahn --upgrade
 pip3 install pycrypto --upgrade
 pip3 install pyserial --upgrade
 pip3 install spidev --upgrade
-pip3 install smbus --upgrade
+sudo apt-get install python3-smbus
 pip3 install matplotlib --upgrade
 
 #install python services
